681dc74f6cf0bb5e4a8d1f395b82185cae7661ca
Luki <Luki@Luki-Komputer>
2018-05-15 12:22:57 +0200

    zadanie 4

--- OOPL9/src/main/java/pl/edu/ur/oopl9/Main.java
+++ OOPL9/src/main/java/pl/edu/ur/oopl9/Main.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pl.edu.ur.oopl9;
 
 import java.util.ArrayList;
@@ -12,27 +7,17 @@ import java.util.List;
 import java.util.Random;
 import java.util.TreeSet;
 
-/**
- *
- * @author maarchyl
- */
+
 public class Main {
 
-    /**
-     *
-     * @param tablica
-     */
+
     static void print(int[] tab){
         for(int i = 0; i < tab.length; i++){
             System.out.println(" " + tab[i]);
             }
         }
-  
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
         int[] tab = new int[100];
         Random generator = new Random();
         for(int i = 0; i < tab.length; i++){
@@ -42,7 +27,7 @@ public class Main {
         Arrays.sort(tab);
         print(tab);
         
-        System.out.println("-----------Zadanie 2-------------");
+        System.out.println("zad2");
         
         TreeSet x = new TreeSet <> ();
         for(int i =0; i < 100; i++){
@@ -55,12 +40,12 @@ public class Main {
             System.out.println(it.next());  
         }  
         
-        System.out.println("-----------Zadanie 3-------------");
+        System.out.println("zad3");
         
         List<Samochod> lista = new ArrayList<>();
-        Samochod s1 = new Samochod("Łada", "Samara", 180, 10000);
-        Samochod s2 = new Samochod("Fiat", "126p", 120, 5000);
-        Samochod s3 = new Samochod("Czerwony", "Najszybszy", 300, 1000000);
+        Samochod s1 = new Samochod("syrena", "morskaxD", 180, 10000);
+        Samochod s2 = new Samochod("fiat", "126p", 120, 5000);
+        Samochod s3 = new Samochod("nissan", "GTR", 300, 1000000);
         lista.add(s1);
         lista.add(s2);
         lista.add(s3);
--- OOPL9/src/main/java/pl/edu/ur/oopl9/Zadanie4.java
+++ OOPL9/src/main/java/pl/edu/ur/oopl9/Zadanie4.java
@@ -1,54 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pl.edu.ur.oopl9;
 
 import java.util.HashMap;
 import java.util.Scanner;
 
-/**
- *
- * @author Patrycja
- */
 public class Zadanie4 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
+        
         HashMap<String, String> slowa = new HashMap<>();
+        slowa.put("tata", "dad");
+        slowa.put("mama", "mom");
         slowa.put("kot", "cat");
         slowa.put("pies", "dog");
         slowa.put("czlowiek", "human");
         slowa.put("kobieta", "woman");
         slowa.put("mezczyna", "man");
-        slowa.put("jesc", "eat");
+        slowa.put("zwierze", "animal");
         slowa.put("pic", "drink");
         slowa.put("spac", "sleep");
         slowa.put("grac", "play");
-        slowa.put("czarny", "black");
-        slowa.put("bialy", "white");
-        slowa.put("czerwony", "red");
-        slowa.put("zielony", "green");
+        slowa.put("jesc", "eat");
+        slowa.put("szafa", "wardrobe");
+        slowa.put("powtorz", "repeat");
+        slowa.put("trawa", "grass");
         slowa.put("lozko", "bed");
         slowa.put("warzywa", "vegetables");
         slowa.put("owoce", "fruit");
-        slowa.put("chomik", "hamster");
-        slowa.put("sklep", "shop");
-        slowa.put("tata", "dad");
-        slowa.put("mama", "mom");
-        
+        slowa.put("gaz lzawiacy", "tear gas");
+        slowa.put("sklep", "shop"); 
         Scanner input = new Scanner(System.in);
         String slowo = null;
-        System.out.println("Wpisz jedno z podanych słów aby otrzymać tłumaczenie: \n kot\n pies\n czlowiek\n kobieta\n mezczyzna\n jesc\n pic\n spac\n grac\n czarny\n bialy\n czerwony\n zielony\n lozko\n warzywa\n owoce\n chonik\n sklep\n tata\n mama");
+        System.out.println("Wpisz jedno z podanych slow aby otrzymac tlumaczenie: \n tata\t mama\t kot\t pies\t czlowiek\t kobieta\t mezczyzna\t zwierze\t pic\t spac\n grac\t jesc\t szafa\t powtorz\t trawa\t lozko\t warzywa\t owoce\t gaz lzawiacy\t sklep\n-------------------------------------------------------------------------\n");
         do {
-        
-        slowo = input.nextLine();
-        System.out.println("" + slowo + " ---- " + slowa.get(slowo));
+            slowo = input.nextLine();
+            System.out.println("" + slowo + " - " + slowa.get(slowo));
         }
         while( !"koniec".equals(slowo));
     }
-
 }
